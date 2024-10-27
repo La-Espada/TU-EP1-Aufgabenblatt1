25ab6ac98f38ed3c73f74f109ce58634dae6fd31
Cemil Aslan <cemil.onepiece@hotmail.com>
2024-10-27 09:29:53 +0100

    finished Aufgabe 2

--- Aufgabenblatt1/src/Aufgabe2.java
+++ Aufgabenblatt1/src/Aufgabe2.java
@@ -4,8 +4,79 @@
 public class Aufgabe2 {
 
     public static void main(String[] args) {
+        String text = "Eine nennenswerte und geeignete Sprache.";
+        String text1 = "Anzahl der Zeichen ist nicht genug!";
+        System.out.println("Aufgabe A");
+        AufgabeA(text,text1);
+        System.out.println("Aufgabe B");
+        AufgabeB(text,text1);
+        System.out.println("Aufgabe C");
+        AufgabeC();
 
         // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+    }
+
+    public static void AufgabeA(String text, String text1){
+        int i =0;
+        int j = 0;
+        String ausgabe1="";
+        String ausgabe2="";
+        while(i<text.length()){
+            if(text.charAt(i) == 'a' || text.charAt(i) == 'A' || text.charAt(i) == 'e' || text.charAt(i) == 'E' || text.charAt(i) == 'i' || text.charAt(i) == 'I' || text.charAt(i) == 'o' || text.charAt(i) == 'O' ||text.charAt(i) == 'u' || text.charAt(i) == 'U'){
+                ausgabe1 += "-" + text.charAt(i);
+            }
+            else {
+                ausgabe1 += text.charAt(i);
+            }
+           i++;
+        }
+        System.out.println(ausgabe1);
+
+        while(j<text1.length()){
+            if(text1.charAt(j) == 'a' || text1.charAt(j) == 'A' || text1.charAt(j) == 'e' || text1.charAt(j) == 'E' || text1.charAt(j) == 'i' || text1.charAt(j) == 'I' || text1.charAt(j) == 'o' || text1.charAt(j) == 'O' || text1.charAt(j) == 'u' || text1.charAt(j) == 'U'){
+                ausgabe2 += "-" + text1.charAt(j);
+            }
+            else {
+                ausgabe2 += text1.charAt(j);
+            }
+            j++;
+        }
+        System.out.println(ausgabe2);
+    }
+
+    private static void AufgabeB(String text, String text1){
+        int i = 0;
+        int j = 0;
+        String ausgabe1="";
+        String ausgabe2="";
+
+        while(i<text.length()){
+            if((text.charAt(i) > 'j') && (text.charAt(i) <='z') || (text.charAt(i) >'J' && text.charAt(i)<='Z')){
+                ausgabe1 += text.charAt(i);
+            }
+            i++;
+        }
+        System.out.println(ausgabe1);
+
+        while(j<text1.length()){
+            if((text1.charAt(j) > 'j') && (text1.charAt(j) <='z') || (text1.charAt(j) >'J' && text1.charAt(j)<='Z')){
+                ausgabe2 += text1.charAt(j);
+            }
+            j++;
+        }
+        System.out.println(ausgabe2);
+    }
+
+    private static void AufgabeC(){
+        int i = 35;
+        String ausgabe = "";
+        while(i<175){
+            if(i%5 == 0 && i%7 == 0){
+                ausgabe = ausgabe + i + " ";
+            }
+            i++;
+        }
+        System.out.println(ausgabe);
     }
 }
 
--- Aufgabenblatt1/src/Aufgabe3.java
+++ Aufgabenblatt1/src/Aufgabe3.java
@@ -10,5 +10,34 @@ public class Aufgabe3 {
     public static void main(String[] args) {
 
         // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        fuchsHasenSimulation(1500,2500,2);
+    }
+
+    public static void fuchsHasenSimulation(int maxCap, int numRabbits, int numFoxes){
+        int dt = 1;
+        double freeCap;
+        double incRabbits;
+        double decFoxes;
+        double numContacts;
+        double numRabbitsA;
+        double numFoxesA;
+
+        for (int i = 0; i < 500; i++){
+            freeCap = maxCap - numRabbits;
+            incRabbits = (1.0 / maxCap) * freeCap * 0.08 * numRabbits;
+            decFoxes = 0.2 * numFoxes;
+            numContacts = numRabbits * numFoxes;
+            numRabbitsA = numRabbits + dt * (incRabbits - 0.002 * numContacts);
+            numFoxesA = numFoxes + dt * (0.0004 * numContacts - decFoxes);
+            System.out.println("Iteration: " + i + " freeCap: " + freeCap + " numRabbits: " + numRabbitsA + " numFoxes: " + numFoxesA);
+        }
+    }
+
+    public static void generateStar(double hasenAnzahl){
+
+    }
+
+    public static void generateHashTag(){
+
     }
 }
